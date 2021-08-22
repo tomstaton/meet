4ae717c89481d456617deaa664620239e95952c4
tomstaton <74940768+tomstaton@users.noreply.github.com>
2021-08-22 16:28:03 -0400

    4.8

--- src/api.js
+++ src/api.js
@@ -66,6 +66,12 @@ export const getEvents = async () => {
     return mockData;
   }
 
+  if (!navigator.onLine) {
+    const events = localStorage.getItem("lastEvents");
+    NProgress.done();
+    return events ? JSON.parse(events).events : [];
+  }
+
   const token = await getAccessToken();
 
   if (token) {
